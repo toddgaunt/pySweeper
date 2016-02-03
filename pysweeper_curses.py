c6cf34ee3bee2406a90501847bdf14f5e88f7058
todd <toddgaunt@protonmail.ch>
2016-02-03 07:15:20 -0500

    got basic functionality of the curses module, just displaying the print_brd() string

--- pysweeper_curses.py
+++ pysweeper_curses.py
@@ -3,28 +3,53 @@
 # apache 2.0
 # minesweeper game
 
-import pysweeper
+from pysweeper import Board, Cell
 import curses
+import time
 
-class pyCurses(curses.initscr()):
-    def __init__(self):
-        #curses.noecho()
-        #curses.cbreak()
-        #self.keypad(1)
+class appGUI():
+    def __init__(self, stdscr):
+        """Draws all main windows the program uses"""
+        # Initialize class variables and assign stdscr
+        self.stdscr = stdscr
+
+        # Actually creating minesweeper board
+        self.mine_brd = Board()
+        self.mine_brd.plant_mines()
+        self.mine_brd.count_surrounding()
+
+        # Creates main window
+        self.stdscr.clear()
+        curses.noecho()
+        curses.cbreak()
+        self.stdscr.keypad(True)
 
     def kill(self):
+        # Kills main window
         curses.nocbreak()
-        self.keypad(0)
+        self.stdscr.keypad(False)
         curses.echo()
         curses.endwin()
-    def say_hello():
-        print ("hello")
 
+    def brdscr(self):
+        begin_x = 20; begin_y = 7
+        height = 100; width = 100
+        brdscr = curses.newwin(height, width, begin_y, begin_x)
 
-def main():
-    stdscr = pyCurses()
-    #stdscr.kill()
-    stdscr.say_hello()
+        for y in range(self.mine_brd.y_length):
+            for x in range(self.mine_brd.x_length):
+                self.mine_brd.cell_flip(y, x)
+                brdscr.addstr(self.mine_brd.print_brd())
+                brdscr.refresh()
+                brdscr.erase()
+
+def main(stdscr):
+    gui = appGUI(stdscr)
+    gui.brdscr()
+    time.sleep(10)
+    gui.kill()
+
 
 if __name__ == "__main__":
-    main()
+   curses.wrapper(main)
+
