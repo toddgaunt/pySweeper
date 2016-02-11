6332000eb9ce9ba5a996b9bd2299a7470d0405dc
todd <toddgaunt@protonmail.ch>
2016-02-11 13:52:27 -0500

    gameplay works! Now just to add game_finality

--- pysweeper_curses.py
+++ pysweeper_curses.py
@@ -7,45 +7,42 @@ from pysweeper import Board, Cell
 from curses import wrapper
 import curses
 import time
+import re
 
 def main(stdscr):
-    #stdscr = curses.initscr()
+    # Sets initial terminal properties
     stdscr.clear()
     curses.noecho()
     curses.cbreak()
     curses.curs_set(0)
-
-    # Allows terminals without color to function
     if curses.has_colors():
         curses.start_color()
 
-    # Color pairs (fg, bg)
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
-    curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    # Sets Color pairs (fg, bg)
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(5, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 
     # Adds title and then colors whole line
     stdscr.addstr("Pysweeper 1.0", curses.A_REVERSE)
     stdscr.chgat(-1, curses.A_REVERSE)
 
     # Adds menu at bottom
+    # subwindow makes updating clean
     text_window = curses.newwin(3,curses.COLS,curses.LINES-3,0)
-    text_window.addstr(1,2, "Press r to start the game, q to quit.")
-    text_window.chgat(1,2,curses.COLS-4,curses.color_pair(3) | curses.A_REVERSE)
     text_window.box()
+    text_sub_window = text_window.subwin(1, curses.COLS-4, curses.LINES-2,2)
+    text_sub_window.addstr(0,0, "Press r to start the game, q to quit.")
+    text_sub_window.chgat(0,0,curses.COLS-4,curses.color_pair(4))
 
     # Main window for holding the game board
+    # subwindow makes updating clean
     game_window = curses.newwin(curses.LINES-4,curses.COLS,1,0)
-
-    # Subwindow for the game board that makes updating clean
-    game_board_window = game_window.subwin(curses.LINES-6, curses.COLS-3, 2,2)
-
-    # Inital text for subwindow
-    game_board_window.addstr("Game goes here.")
-
-    # Draws a border around the main window
     game_window.box()
+    game_board_window = game_window.subwin(curses.LINES-6, curses.COLS-3, 2,2)
+    game_board_window.addstr("Welcome to pysweeper!")
 
     # Update internal window data structures
     stdscr.noutrefresh()
@@ -60,7 +57,6 @@ def main(stdscr):
         player_input = game_window.getch()
 
         if player_input == ord('r') or player_input == ord('R'):
-
             # Inializes game board
             mine_brd = Board()
             mine_brd.plant_mines()
@@ -68,20 +64,26 @@ def main(stdscr):
 
             # Generation message
             game_board_window.clear()
-            game_board_window.addstr('You pressed r, adding game board.', curses.color_pair(3))
+            game_board_window.addstr('Generating a board with {} length and {} height with {} mines.'.format(mine_brd.x_length, mine_brd.y_length, mine_brd.mine_count), curses.color_pair(3))
             game_board_window.refresh()
             time.sleep(1)
 
             # Some debugging code
-            for y in range(mine_brd.y_length):
+            """for y in range(mine_brd.y_length):
                 for x in range(mine_brd.x_length):
-                    mine_brd.flip_cell(y,x)
+                    mine_brd.flip_cell(y,x)"""
 
-            # Adds the actual tiles of the board to the window
-            game_board_window.clear()
-            add_brd_str(mine_brd, game_board_window)
-            game_board_window.refresh()
-            get_coords(text_window)
+            ## Game loop
+            # prints board to curses window, then calls function to grab user input
+            while True:
+                add_brd_str(mine_brd, game_board_window)
+                coords = get_coords(text_sub_window, mine_brd)
+                x = int(coords[0])
+                y = int(coords[1])
+                if coords[2] == 'f':
+                    mine_brd[y][x].flag()
+                else:
+                    mine_brd.flip_cell(y,x)
 
         elif player_input == ord('q') or player_input == ord('Q'):
             playing = False
@@ -90,19 +92,57 @@ def main(stdscr):
         stdscr.noutrefresh()
         game_window.noutrefresh()
         game_board_window.noutrefresh()
+        text_window.noutrefresh()
+        text_sub_window.noutrefresh()
         curses.doupdate()
 
     # End of program
     restore_term()
 
-def get_coords(window):
-    window.clear
-    window.addstr(1,2, "Enter x and y coordinates: ")
-    window.chgat(1,2,curses.COLS-4, curses.color_pair(2) | curses.A_REVERSE)
-    coords = window.getstr()
-    window.refresh
+def get_coords(window, board):
+    # regex for seperating coordinates and f flag into groups
+    xyf = re.compile(r"([0-9]+),([0-9]+)(f?)")
+    while True:
+        window.clear()
+        window.addstr(0,0, "Enter x and y coordinates: x,y")
+        window.chgat(0,0,curses.COLS-4, curses.color_pair(3))
+        # Takes string for coordinates
+        curses.echo()
+        caught_str = window.getstr(0,27).decode(encoding="utf-8")
+        curses.noecho()
+        window.refresh()
+        # Compares
+        re_match = xyf.match(str(caught_str))
+        # If no match, print error message
+        if re_match == None:
+            window.clear()
+            window.addstr(0,0,"That was not a proper entry, eg. x,y or x,yf to flag")
+            window.chgat(0,0,curses.COLS-4, curses.color_pair(2) | curses.A_REVERSE)
+            window.refresh()
+            time.sleep(1)
+            continue
+        # If coordinates are out of bounds, print error message
+        elif int(re_match.group(1)) >= board.x_length or int(re_match.group(2)) >= board.y_length:
+            window.clear()
+            window.addstr(0,0,"Those coordinates are out of bounds!")
+            window.chgat(0,0,curses.COLS-4, curses.color_pair(2) | curses.A_REVERSE)
+            window.refresh()
+            time.sleep(1)
+            continue
+        # If every test is passed, return coordinates as a 3-index list
+        else:
+            window.clear()
+            window.addstr(0,0,re_match.group(0) + " are valid coordinates")
+            window.chgat(0,0,curses.COLS-4, curses.color_pair(3))
+            window.refresh()
+            coords = []
+            coords.append(re_match.group(1))
+            coords.append(re_match.group(2))
+            coords.append(re_match.group(3))
+            return coords
 
 def add_brd_str(board, window):
+    window.clear()
     """Displays all cells of the array into a curses window"""
     y_flip = board.y_length - 1
     for y in range(board.y_length):
@@ -110,19 +150,22 @@ def add_brd_str(board, window):
         for x in range(board.x_length):
             if board.get_cell(y_flip,x).revealed:
                 tile = board.get_cell(y_flip,x).get_tile()
+                if tile == 'X':
+                    tile_color = 2
+                elif tile == 0:
+                    tile_color = 1
+                elif tile == 1:
+                    tile_color = 4
+                elif tile == 2:
+                    tile_color = 3
+                else:
+                    tile_color = 5
             elif board.get_cell(y_flip,x).flagged == True:
                 tile = "f"
+                tile_color = 1
             else:
                 tile = "#"
-
-            if tile == "X":
                 tile_color = 1
-            elif tile == 1:
-                tile_color = 2
-            elif tile >= 2:
-                tile_color = 3
-            else:
-                tile_color = 4
             window.addstr(y + (curses.LINES//6),
                           2 + (x * 2) + (curses.COLS//3),
                           str(tile),
@@ -132,6 +175,7 @@ def add_brd_str(board, window):
     for x in range(board.x_length):
         window.addstr(1 + y + (curses.LINES//6), 2 + (x * 2) + (curses.COLS//3), "|")
         window.addstr(2 + y + (curses.LINES//6), 2 + (x * 2) + (curses.COLS//3), str(x))
+    window.refresh()
 
 def restore_term():
     # Restore terminal settings
