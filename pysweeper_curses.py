1bc365190dcb0a0bc6444a438f57f3fb998fbcb4
todd <toddgaunt@protonmail.ch>
2016-02-17 17:02:15 -0500

    added terminal resizing, at the cost of messy code...

--- pysweeper_curses.py
+++ pysweeper_curses.py
@@ -8,7 +8,9 @@ from curses import wrapper
 import curses
 import time
 import re
-#TODO allow for terminal resizing
+#TODO Refactor to make cleaner
+#TODO extend curses windows class to enable window naming, so its easy to lookup window without
+# caring about it's index number
 
 class AppUI(object):
     def __init__(self, stdscr, playing=True, win=False, win_counter=0, loss_counter=0, game_time=0):
@@ -18,19 +20,11 @@ class AppUI(object):
         self.win_counter = 0
         self.loss_counter = 0
         self.game_time = 0
+        self.windows = []
+        self.sub_windows = []
 
         # Creating UI windows
         self.stdscr = stdscr
-
-        # board_sub_window holds game_board string
-        self.board_window = curses.newwin(curses.LINES-4,curses.COLS,1,0)
-        self.board_window.box()
-        self.board_sub_window = self.board_window.subwin(curses.LINES-6, curses.COLS-3, 2,2)
-
-        # text_sub_window holds user prompt messages
-        self.text_window = curses.newwin(3,curses.COLS,curses.LINES-3,0)
-        self.text_window.box()
-        self.text_sub_window = self.text_window.subwin(1, curses.COLS-4, curses.LINES-2,2)
 
         # Sets initial terminal properties
         self.stdscr.clear()
@@ -47,21 +41,23 @@ class AppUI(object):
         curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
         curses.init_pair(5, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 
+        # Draws all main windows
+        self.draw_windows()
+
+    def create_window(self, nlines, ncols, beginy, beginx):
+        """Function that creates a curses.window object and appends it to an array of windows"""
+        self.windows.append(curses.newwin(nlines, ncols, beginy, beginx))
+
+    def create_sub_window(self, window, nlines, ncols, beginy, beginx):
+        """Function that creates a window.sub_window object and appends it to an array of sub windows"""
+        self.sub_windows.append(self.windows[window].subwin(nlines, ncols, beginy, beginx))
+
+    def title_message(self, window):
         # Adds title and then colors whole line to top of stdscr
-        self.stdscr.addstr(0,0, "Pysweeper 1.1", curses.A_REVERSE)
-        self.stdscr.chgat(-1, curses.A_REVERSE)
+        window.addstr("Pysweeper 1.1", curses.A_REVERSE)
+        window.chgat(-1, curses.A_REVERSE)
 
-        # Update internal window data structures
-        self.stdscr.noutrefresh()
-        self.board_window.noutrefresh()
-        self.board_sub_window.noutrefresh()
-        self.text_window.noutrefresh()
-        self.text_sub_window.noutrefresh()
-
-        # Redraw the screen
-        curses.doupdate()
-
-    def start_prompt(self, window):
+    def prompt_message(self, window):
         """Prompts the user to start playing, and returns a character they input"""
         window.clear()
         window.addstr(0,0, "Press r to start the game, q to quit.")
@@ -69,7 +65,7 @@ class AppUI(object):
         window.refresh()
         return window.getch()
 
-    def game_info(self, window):
+    def info_message(self, window):
         window.clear()
         window.addstr("\nWin = {}. You have {} wins and {} losses.".format(self.win, self.win_counter, self.loss_counter))
         window.refresh()
@@ -168,13 +164,40 @@ class AppUI(object):
         elif self.win == False:
             self.loss_counter += 1
 
-    def refresh_all(self):
+    def draw_windows(self):
         """Refreshes all window in class from the bottom up"""
-        self.stdscr.noutrefresh()
-        self.board_window.noutrefresh()
-        self.board_sub_window.noutrefresh()
-        self.text_window.noutrefresh()
-        self.text_sub_window.noutrefresh()
+        """ Contains predfined properties of main UI windows"""
+        # Resizes windows to term size
+        y, x = self.stdscr.getmaxyx()
+        curses.resize_term(y, x)
+        curses.KEY_RESIZE
+
+        # Resets window arrays
+        self.windows = []
+        self.sub_windows = []
+
+        # Board window and sub window
+        self.create_window(curses.LINES-4, curses.COLS,1,0)
+        self.create_sub_window(0, curses.LINES-7, curses.COLS-3, 2, 2)
+        self.windows[0].box()
+
+        # Text window and sub window
+        self.create_window(3, curses.COLS, curses.LINES-3, 0)
+        self.create_sub_window(1, 1, curses.COLS-4, curses.LINES-2,2)
+        self.windows[1].box()
+
+        # Window for holding title
+        self.create_window(1, curses.COLS, 0, 0)
+
+        # draws title message
+        self.title_message(self.windows[2])
+
+        self.stdscr.addstr(0,0, "Pysweeper 1.1", curses.A_REVERSE)
+        self.stdscr.chgat(-1, curses.A_REVERSE)
+        # Update internal window data structures
+        for i in range(len(self.windows)):
+            self.windows[i].noutrefresh()
+
         # Redraws the screen
         curses.doupdate()
 
@@ -190,43 +213,42 @@ class AppUI(object):
 def main(stdscr):
     UI = AppUI(stdscr)
     while UI.playing:
-        player_input = UI.start_prompt(UI.text_sub_window)
+        player_input = UI.prompt_message(UI.sub_windows[1])
         if player_input == ord('r') or player_input == ord('R'):
             # Initializes board
             mine_brd = Board()
 
             # Board generation message
-            UI.board_sub_window.clear()
-            UI.board_sub_window.addstr('Generating a board with {} length and {} height with {} mines.'.format(mine_brd.x_length, mine_brd.y_length, mine_brd.mined_tiles), curses.color_pair(3))
-            UI.board_sub_window.refresh()
+            UI.sub_windows[0].clear()
+            UI.sub_windows[0].addstr('Generating a board with {} length and {} height with {} mines.'.format(mine_brd.x_length, mine_brd.y_length, mine_brd.mined_tiles), curses.color_pair(3))
+            UI.sub_windows[0].refresh()
             time.sleep(1)
 
             # Initializes screen with game board
             # Doesn't plant any mines or increment tiles until first coordinate has been chosen
-            UI.add_brd_str(window=UI.board_sub_window, board=mine_brd)
-            coords = UI.get_coords(window=UI.text_sub_window, board=mine_brd)
+            UI.add_brd_str(window=UI.sub_windows[0], board=mine_brd)
+            coords = UI.get_coords(window=UI.sub_windows[1], board=mine_brd)
             mine_brd.plant_mines()
             mine_brd.count_surrounding()
             game_over = UI.parse_coords(coords, mine_brd)
-
             game_over = False
             # prints board to curses window, then calls function to grab user input
             while True:
-                UI.add_brd_str(window=UI.board_sub_window, board=mine_brd)
-                coords = UI.get_coords(window=UI.text_sub_window, board=mine_brd)
+                UI.draw_windows()
+                UI.add_brd_str(window=UI.sub_windows[0], board=mine_brd)
+                coords = UI.get_coords(window=UI.sub_windows[1], board=mine_brd)
                 game_over = UI.parse_coords(coords, mine_brd)
                 if game_over:
                     break
             # Increments wins and losses
             UI.increment_stats()
             # Prints game info to player after game is over
-            UI.game_info(UI.board_sub_window)
+            UI.info_message(UI.sub_windows[0])
 
         elif player_input == ord('q') or player_input == ord('Q'):
             UI.playing = False
 
-        # Updates all windows
-        UI.refresh_all
+        UI.draw_windows()
     # End of program
     UI.restore_term()
 
